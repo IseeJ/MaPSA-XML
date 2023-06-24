02edbbeb3023931f31faf68801583140166e5494
IseeJ <122057875+IseeJ@users.noreply.github.com>
2023-06-24 13:21:41 -0400

    Update dict2xmlpy.py

--- dict2xmlpy.py
+++ dict2xmlpy.py
@@ -5,8 +5,21 @@ data = {
             'KIND_OF_PART': 'MaPSA',
             'NAME_LABEL': 'AEM_35494_016L',
             'LOCATION': 'AEMtec',
-            'VERSION': '2.0'
+            'VERSION': '2.0',
+            'CHILDREN':{
+                'PART mode="auto"':{
+                    'KIND_OF_PART':'MPA Chip',
+                    'SERIAL_NUMBER':'805315419',
+                    'NAME_LABEL':'N6Y215_03_506'
+                },
+            'PREDEFINED_ATTRIBUTES':{
+                'ATTRIBUTE':{
+                    'NAME':'Chip Posn on Sensor',
+                    'VALUE':'1'
+                }
+                }
+            }
         }
     }
   }
-print (d2x(data, wrap="all", indent="  "))
+print(d2x(data, wrap="all", indent="  "))
